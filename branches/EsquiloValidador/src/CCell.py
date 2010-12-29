568e34626a62da1aa3e75a41358aea07b1f2ff73
matherthal <matherthal@a613a8f4-758b-7c5e-7efa-89749f2c3fff>
2010-12-29 05:05:55 +0000

    1o commit. Já foi implementado uma comparação entre os valores de Receita, Despesa e Saldo entre as duas planilhas (CASH e ccespecie). Como o nome desses arquivos costuma mudar, comecei a implementar umas janelas para adquirir esses nomes.

---
+++ branches/EsquiloValidador/src/CCell.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
+#Esta classe contém não somente o valor e tipo da célula, como sua localização
+class ComplCell:
+    def __init__(self, cell):
+        self._value = cell.value
+        self._ctype = cell.ctype
+        self._row = None
+        self._col = None
+    
+    def setPos(self, row, col):
+        self._row = row
+        self._col = col
+        
+    def getPos(self):
+        return (self._row,self._col)
+    
+    def setRow(self, row):
+        self._row = row
+        
+    def getRow(self):
+        return self._row
+    
+    def setCol(self, col):
+        self._col = col
+        
+    def getCol(self):
+        return self._col
+
+    def setValue(self, value):
+        self._value = value
+        
+    def getValue(self):
+        return self._value
+        
+    def setType(self, type):
+        self._ctype = type
+        
+    def getType(self):
+        return self._ctype
